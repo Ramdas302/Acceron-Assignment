5dcb1ccd5e49205bad59682b1c484661fc3d9102
Ramdas302 <63226885+Ramdas302@users.noreply.github.com>
2024-09-29 19:14:12 +0530

    Update database.js

--- db/database.js
+++ db/database.js
@@ -5,6 +5,6 @@ url = 'mongodb://localhost:27017/order_products';
 mongoose.connect(url).then(()=>{
 console.log('mongodb connected successfully')
 }).catch(()=>{
-    console.log('mongodb not connected please check')
+    console.log('mongodb not connected please check now')
 })
 module.exports=mongoose;
